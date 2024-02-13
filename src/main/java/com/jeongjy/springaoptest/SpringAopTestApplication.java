a97ca345875a1411d412cae0fbcee0d852996185
정준영 <jeongjy@mrblue.com>
2024-02-14 08:31:53 +0900

    init

--- src/main/java/com/jeongjy/springaoptest/SpringAopTestApplication.java
+++ src/main/java/com/jeongjy/springaoptest/SpringAopTestApplication.java
@@ -1,4 +1,4 @@
-package com.mrblue.springaoptest;
+package com.jeongjy.springaoptest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
