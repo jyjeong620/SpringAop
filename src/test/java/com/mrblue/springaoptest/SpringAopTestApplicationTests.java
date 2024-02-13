68ec39f7a1b144936804eec20454bce5610a06b2
정준영 <jeongjy@mrblue.com>
2024-02-14 08:30:22 +0900

    Initial commit

---
+++ src/test/java/com/mrblue/springaoptest/SpringAopTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrblue.springaoptest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
